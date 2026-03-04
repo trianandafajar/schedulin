aff571c9afafec0838926e41cacf76b964be0fd7
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-03-04 08:15:21 +0700

    fix: notif dummy

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -110,7 +110,7 @@ const AppHeader: React.FC = () => {
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
             <ThemeToggleButton />
-           <NotificationDropdown /> 
+           {/* <NotificationDropdown />  */}
           </div>
           <UserDropdown /> 
      
